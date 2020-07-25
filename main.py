8471da7b05a2258fe82cae52de1ab6d0c1506cb8
Ethan Steidl <EthanSteidl@goldenwest.com>
2020-07-25 14:19:36 -0600

    removed some temp files and started the file for machine learning

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def update_data(endpoints):
 
   #get current year
   current_year = datetime.now().year
-
+  flag = 0
 
   #make requests and store in dictionaries between 1922 and current year in 10yr incs
   while (current_year-9 > 1922):
-    '''
+    
     headers = {'Content-type': 'application/json'}
     data = json.dumps({"seriesid": endpoints,"startyear": str(current_year-9), "endyear": str(current_year)})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
@@ -24,7 +24,7 @@ def update_data(endpoints):
     filename = 'p' + str(current_year) + '.txt'
     with open(filename,'w') as fout:
       fout.write(p.text)
-      '''
+      
     with open('p2020.txt','r') as fin:
       words = fin.read()
     json_data = json.loads(words)
@@ -36,16 +36,19 @@ def update_data(endpoints):
     trunk_data = json_data['Results']['series']
 
     for i in range(len(endpoints)):
-      print(endpoints[i])
+      print('adding endpoint: '+ endpoints[i] + ' ' + str(current_year))
 
       ordered_data[i].extend(trunk_data[i]['data'])
+      if (flag == 1):
+        print( ordered_data[0])
+        
     current_year = current_year - 10
 
-
+    flag = flag + 1
   
   for i in range(len(endpoints)):
     fout = open(endpoints[i], 'w')
-    fout.write(ordered_data)
+    fout.write(str(ordered_data[i]))
     fout.close()
   
   return
